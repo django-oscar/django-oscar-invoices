38015a4d66249823c8197c3cd0b12fb6191bfa5b
Alexander Gaevsky <sasha@sasha0.ru>
2018-06-07 15:16:06 +0300

    Fix package paths determination, drop Django 1.11 and Oscar 1.5 support.

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from setuptools import find_packages, setup
 
-install_requires = [
-    'django>=1.11,<2.1',
-    'django-oscar>=1.5',
-]
-
 setup(
     name='django-oscar-invoices',
     url='https://github.com/oscaro/django-oscar-invoices',
@@ -13,27 +8,23 @@ setup(
     description='Invoices generation for Django Oscar',
     long_description=open('README.md').read(),
     license='BSD',
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
+    packages=find_packages(exclude=['sandbox*', 'tests*']),
     include_package_data=True,
+    zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: Unix',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
     install_requires=[
         'django>=1.11,<2.1',
-        'django-oscar>=1.5',
+        'django-oscar>=1.6',
     ],
 )
