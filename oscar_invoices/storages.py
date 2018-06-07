2a5c12d533ed449baba432e5e128455fd8cd97ff
Alexander Gaevsky <sasha@sasha0.ru>
2018-06-07 15:42:50 +0300

    Fix application settings usage in the document storage.

--- oscar_invoices/storages.py
+++ oscar_invoices/storages.py
@@ -1,7 +1,8 @@
-from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.utils.deconstruct import deconstructible
 from django.utils.functional import cached_property
+
+from . import app_settings
 
 
 @deconstructible
@@ -13,10 +14,10 @@ class DocumentsStorage(FileSystemStorage):
 
     def __init__(self, *args, **kwargs):
         super(DocumentsStorage, self).__init__(*args, **kwargs)
-        self._location = settings.OSCAR_DOCUMENTS_ROOT
+        self._location = app_settings.OSCAR_INVOICES_DOCUMENTS_ROOT
 
     def _clear_cached_properties(self, setting, **kwargs):
-        if setting == 'OSCAR_DOCUMENTS_ROOT':
+        if setting == 'OSCAR_INVOICES_DOCUMENTS_ROOT':
             self.__dict__.pop('base_location', None)
             self.__dict__.pop('location', None)
 
@@ -28,7 +29,7 @@ class DocumentsStorage(FileSystemStorage):
 
     @cached_property
     def base_location(self):
-        return self._value_or_setting(self._location, settings.OSCAR_DOCUMENTS_ROOT)
+        return self._value_or_setting(self._location, app_settings.OSCAR_INVOICES_DOCUMENTS_ROOT)
 
     def url(self, name):
         raise ValueError("This file is not accessible via a URL.")
