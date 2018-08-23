505a87745ff2e9fa5d752e5930679430c246a779
Basil Dubyk <samitnuk.work@gmail.com>
2018-08-23 13:48:05 +0300

    Update urls configuration for development mode

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import i18n
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path('i18n/', include(i18n)),
     path('', application.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
