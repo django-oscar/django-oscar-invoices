12fbf6ddc330a446c4d694e303bc25e4b72a8e9c
Lars van de Kerkhof <lars@permanentmarkers.nl>
2019-01-24 17:54:19 +0100

    better to use staff_member_required

--- oscar_invoices/views.py
+++ oscar_invoices/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
-from django.contrib.auth.decorators import user_passes_test
-
+from django.contrib.admin.views.decorators import staff_member_required
 from oscar.core.loading import get_model
 
 from .utils import InvoiceCreator
@@ -8,7 +7,7 @@ from .utils import InvoiceCreator
 Invoice = get_model('oscar_invoices', 'Invoice')
 
 
-@user_passes_test(lambda user: user.is_superuser)
+@staff_member_required
 def invoice(request, pk):
     iv = Invoice.objects.get(pk=pk)
     ic = InvoiceCreator()
