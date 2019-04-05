37e50e95a2d762c8ea7b3904c78dd25e5d1251d0
Basil Dubyk <samitnuk.work@gmail.com>
2019-04-05 17:41:00 +0300

    Use in tests standard python `assert` for verifying expectations/values

--- tests/test_invoices.py
+++ tests/test_invoices.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 import shutil
 from datetime import date
 from mock import patch
@@ -48,36 +49,36 @@ class TestInvoice(WebTestCase):
     def _test_invoice_is_created(self, order_number='000043'):
         order = create_order(number=order_number, user=self.user)
         InvoiceCreator().create_invoice(order)
-        self.assertTrue(Invoice.objects.exists())
+        assert Invoice.objects.exists()
         invoice = Invoice.objects.first()
         return invoice
 
     def test_invoice_cannot_be_created_without_legal_entity(self):
         LegalEntity.objects.all().delete()
 
-        self.assertFalse(Invoice.objects.exists())
+        assert not Invoice.objects.exists()
         InvoiceCreator().create_invoice(self.order)
-        self.assertFalse(Invoice.objects.exists())
+        assert not Invoice.objects.exists()
 
     def test_invoice_cannot_be_created_without_legal_entity_address(self):
         LegalEntityAddress.objects.all().delete()
 
-        self.assertFalse(Invoice.objects.exists())
+        assert not Invoice.objects.exists()
         InvoiceCreator().create_invoice(self.order)
         self.assertFalse(Invoice.objects.exists())
 
     def test_invoice_can_be_created_with_legal_entity_and_its_address(self):
-        self.assertFalse(Invoice.objects.exists())
+        assert not Invoice.objects.exists()
         InvoiceCreator().create_invoice(self.order)
-        self.assertTrue(Invoice.objects.exists())
+        assert Invoice.objects.exists()
         invoice = Invoice.objects.first()
         # Document created and saved
-        self.assertIsNotNone(invoice.document)
+        assert invoice.document is not None
 
     def test_invoice_creation_based_on_settings(self):
         invoice = self._test_invoice_is_created()
         # Document created and saved
-        self.assertIsNotNone(invoice.document)
+        assert invoice.document is not None
 
     def test_invoice_document_is_not_accessible_via_url(self):
         invoice = self._test_invoice_is_created()
@@ -88,7 +89,7 @@ class TestInvoice(WebTestCase):
 
         # Invoice document is not accessible via url for any user
         for user in [self.user, another_user, staff_user, superuser]:
-            with self.assertRaisesMessage(ValueError, 'This file is not accessible via a URL.'):
+            with pytest.raises(ValueError, match='This file is not accessible via a URL.'):
                 self.app.get(invoice.document.url, user=user)
 
     def test_invoice_was_saved_to_correct_folder(self):
@@ -96,25 +97,25 @@ class TestInvoice(WebTestCase):
         self._test_invoice_is_created(order_number=order_number)
 
         file_names = os.listdir(FULL_PATH_TO_INVOICES)
-        self.assertEqual(len(file_names), 1)  # Only one file here
+        assert len(file_names) == 1  # Only one file here
         invoice_file_name = 'invoice_{}.html'.format(order_number)
-        self.assertIn(invoice_file_name, file_names)
+        assert invoice_file_name in file_names
 
         if os.path.exists(settings.MEDIA_ROOT):
             media_file_names = os.listdir(settings.MEDIA_ROOT)
-            self.assertNotIn(invoice_file_name, media_file_names)
+            assert invoice_file_name not in media_file_names
 
     def test_default_invoice_model_used(self):
         order_number = 'TEST_number_000d6'
         invoice = self._test_invoice_is_created(order_number=order_number)
-        self.assertIsInstance(invoice, Invoice)
+        assert isinstance(invoice, Invoice)
 
     @patch('oscar_invoices.app_settings.OSCAR_INVOICES_INVOICE_MODEL', 'custom_invoices.CustomInvoice')
     def test_custom_invoice_model_used(self, *args, **kwargs):
         order_number = 'TEST_number_000d6'
         order = create_order(number=order_number, user=self.user)
         InvoiceCreator().create_invoice(order)
-        self.assertTrue(CustomInvoice.objects.exists())
+        assert CustomInvoice.objects.exists()
 
     def test_str_method_of_invoice_model_instance(self):
         """
@@ -124,7 +125,7 @@ class TestInvoice(WebTestCase):
         order_number = '0000042'
         order = create_order(number=order_number, user=self.user)
         invoice = InvoiceCreator().create_invoice(order)
-        self.assertEqual(str(invoice), 'Invoice #{} for order #{}'.format(invoice.number, order_number))
+        assert str(invoice) == 'Invoice #{} for order #{}'.format(invoice.number, order_number)
 
     def test_str_method_of_invoice_model_instance_when_order_is_deleted(self):
         """
@@ -137,4 +138,4 @@ class TestInvoice(WebTestCase):
         invoice = InvoiceCreator().create_invoice(order)
         order.delete()
         invoice.refresh_from_db()
-        self.assertEqual(str(invoice), 'Invoice #{}'.format(invoice.number))
+        assert str(invoice) == 'Invoice #{}'.format(invoice.number)
