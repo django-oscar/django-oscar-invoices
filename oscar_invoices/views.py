24f7e583d20662646167920bca51b9f1bbdbc3d3
Lars van de Kerkhof <lars@permanentmarkers.nl>
2019-04-05 16:50:49 +0200

    Invoice preview (#13)
    * Update .gitignore
    
    * Also add python 3.7, pip install -e .[test] also installs the package
    
    * Fixed typo in Django dependencies, make sure tox allows running pytest
    
    * Implemented preview function.
    
    * better to use staff_member_required
    
    * This could fix the build if we are lucky
    
    * new version and maybe avoiding pip_pre will work

---
+++ oscar_invoices/views.py
@@ -0,0 +1,20 @@
+from django.contrib.admin.views.decorators import staff_member_required
+from oscar.core.loading import get_model
+from django.http import HttpResponse
+
+from .utils import InvoiceCreator
+
+Invoice = get_model('oscar_invoices', 'Invoice')
+
+
+@staff_member_required
+def invoice(request, pk):
+    iv = Invoice.objects.get(pk=pk)
+    ic = InvoiceCreator()
+    rendered_template = ic.render_document(
+        iv,
+        legal_entity=iv.legal_entity,
+        order=iv.order,
+        use_path=False,
+    )
+    return HttpResponse(rendered_template)
