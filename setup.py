a645e6cbf1c17fb224e280c82ae054af857cf5df
Alexander Gaevsky <sasha@sasha0.ru>
2019-04-06 10:39:00 +0300

    Add Psycopg2 to the test dependencies.

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     install_requires=[
+        'psycopg2>=2.7',
         'phonenumbers',
         'pillow',
         'django>=1.11,<2.2',
