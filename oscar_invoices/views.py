b68630276b49c435c0e974876dc5988d24e2234e
Basil Dubyk <samitnuk.work@gmail.com>
2019-04-08 09:36:52 +0300

    Use CBV instead of FBV for invoice preview

--- oscar_invoices/views.py
+++ oscar_invoices/views.py
@@ -1,20 +1,27 @@
-from django.contrib.admin.views.decorators import staff_member_required
 from oscar.core.loading import get_model
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.http import HttpResponse
+from django.views.generic import View
+from django.views.generic.detail import SingleObjectMixin
 
 from .utils import InvoiceCreator
 
 Invoice = get_model('oscar_invoices', 'Invoice')
 
 
-@staff_member_required
-def invoice(request, pk):
-    iv = Invoice.objects.get(pk=pk)
-    ic = InvoiceCreator()
-    rendered_template = ic.render_document(
-        iv,
-        legal_entity=iv.legal_entity,
-        order=iv.order,
-        use_path=False,
-    )
-    return HttpResponse(rendered_template)
+class InvoicePreviewView(UserPassesTestMixin, SingleObjectMixin, View):
+    queryset = Invoice.objects.all()
+
+    def test_func(self):
+        user = self.request.user
+        return user.is_staff
+
+    def get(self, request, *args, **kwargs):
+        invoice = self.get_object()
+        rendered_invoice = InvoiceCreator().render_document(
+            invoice=invoice,
+            legal_entity=invoice.legal_entity,
+            order=invoice.order,
+            use_path=False,
+        )
+        return HttpResponse(rendered_invoice)
