81ad4f9b431b61a90a7c5816637b1541c691f2c5
Basil Dubyk <samitnuk.work@gmail.com>
2019-04-08 09:35:46 +0300

    Add `oscar_invoices` URLs to `sandbox` URLs

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -5,11 +5,13 @@ from django.contrib import admin
 from django.urls import include, path
 
 from oscar.app import application
+from oscar_invoices import urls as oscar_invoices_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', include(i18n)),
     path('', application.urls),
+    path('', include(oscar_invoices_urls)),
 ]
 
 if settings.DEBUG:
