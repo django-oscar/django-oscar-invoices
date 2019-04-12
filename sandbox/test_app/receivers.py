4d913b2e2ec8e0eabf2eae36977bf25434ba3cca
Alexander Gaevsky <sasha@sasha0.ru>
2019-04-12 11:33:43 +0300

    Use dynamic invoice creator class import in the signal receiver in the sandbox.

--- sandbox/test_app/receivers.py
+++ sandbox/test_app/receivers.py
@@ -1,8 +1,10 @@
 from django.dispatch import receiver
 
 from oscar.apps.order.signals import order_placed
+from oscar.core.loading import get_class
 
-from oscar_invoices.utils import InvoiceCreator
+
+InvoiceCreator = get_class('oscar_invoices.utils', 'InvoiceCreator')
 
 
 @receiver(order_placed)
