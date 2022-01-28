6230bb8acb27668aaea91eb1e76d9a8b04eac8fb
Viggodevries <viggo@highbiza.nl>
2022-01-28 10:28:57 +0100

    pass the request to the preview and also get everything with get_class

--- oscar_invoices/urls.py
+++ oscar_invoices/urls.py
@@ -1,8 +1,10 @@
 from django.urls import re_path
+from oscar.core.loading import get_class
 
-from . import views
+InvoicePreviewView = get_class("oscar_invoices.views", "InvoicePreviewView")
+
 
 app_name = "oscar_invoices"
 urlpatterns = [
-    re_path(r"invoice/(?P<pk>\d+)/", views.InvoicePreviewView.as_view(), name="invoice"),
+    re_path(r"invoice/(?P<pk>\d+)/", InvoicePreviewView.as_view(), name="invoice"),
 ]
