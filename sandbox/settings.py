00f14cc31c207479100dffaf6f6c57e66efb5d98
Samar Hassan <samarhassanphone@gmail.com>
2026-01-27 10:37:33 +0000

    feat ⭐ add jenkins file

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -216,6 +216,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'sandbox', 'static')
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'sandbox', 'media')
